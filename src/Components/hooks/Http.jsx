fb0e2871a34760efdbf38a3fddd6283f690efbfe
Ravi kumar <rkconsultancy34@gmail.com>
2025-07-24 02:49:39 +0530

    api url updated

--- src/Components/hooks/Http.jsx
+++ src/Components/hooks/Http.jsx
@@ -1,8 +1,8 @@
-// export const apiUrl = 'https://construction-project-backend-lkb5.onrender.com/api';
-// export const fileUrl = 'https://construction-project-backend-lkb5.onrender.com';
+export const apiUrl = 'https://construction-project-backend-lkb5.onrender.com/api';
+export const fileUrl = 'https://construction-project-backend-lkb5.onrender.com';
 
-export const apiUrl = 'http://127.0.0.1:8000/api';
-export const fileUrl = 'http://127.0.0.1:8000';
+// export const apiUrl = 'http://127.0.0.1:8000/api';
+// export const fileUrl = 'http://127.0.0.1:8000';
 export const token = () => {
     const userInfo = localStorage.getItem('userInfo');
     const data = JSON.parse(userInfo)
